7d2a64c3bc97d33e1b696019536f7627bcc3f3c4
1CSH1 <602685158@qq.com>
2017-07-01 23:21:32 +0800

    add ngx-translate module for support i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(public translateService: TranslateService) {
+
+  }
+
+  ngOnInit() {
+    // --- set i18n begin ---
+    this.translateService.addLangs(["zh", "en"]);
+    this.translateService.setDefaultLang("zh");
+    const browserLang = this.translateService.getBrowserLang();
+    this.translateService.use(browserLang.match(/zh|en/) ? browserLang : 'zh');
+    // --- set i18n end ---
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule, Http } from '@angular/http';
+import { TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
+
+export function createTranslateHttpLoader(http: Http) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateHttpLoader),
+        deps: [Http]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
