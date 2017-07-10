1049c3b8e6bf31739ca09bded39069c95b4440dc
1CSH1 <602685158@qq.com>
2017-07-10 22:22:17 +0800

    add categories module

---
+++ src/app/categories/categories.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriesComponent } from './categories.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CategoriesComponent]
+})
+export class CategoriesModule { }
